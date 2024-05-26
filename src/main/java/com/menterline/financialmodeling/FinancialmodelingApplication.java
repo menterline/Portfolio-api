c93cbbd80be13eebc0f558b866dda0751a4cad1e
Matt Enterline <matthew.j.enterline@gmail.com>
2024-05-26 14:34:19 -0400

    Supporting multiple tickers as request parameters

--- src/main/java/com/menterline/financialmodeling/FinancialmodelingApplication.java
+++ src/main/java/com/menterline/financialmodeling/FinancialmodelingApplication.java
@@ -13,6 +13,7 @@ import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.function.Function;
 
 @SpringBootApplication
@@ -33,19 +34,22 @@ public class FinancialmodelingApplication {
 	takes multiple tickers
 	 */
 	@GetMapping("/timeSeriesDaily")
-	public MyTickerData getTickerData(@RequestParam(value = "tickers", defaultValue = "") String[] tickers) {
+	public ArrayList<MyTickerData> getTickerData(@RequestParam(value = "tickers", defaultValue = "") String[] tickers) {
 		//parse url params into array of tickers
 		//call alphavantage API for each ticker
 		//create a mapper that takes AV response to make it something usable - a TickerResponse
         try {
 			Function<String, WebClient> webClientProvider = baseUrl -> WebClient.builder().baseUrl(baseUrl).build();
 			AlphaVantageService service = new AlphaVantageService(webClientProvider);
-
-			AlphaVantageTickerData response = service.getDailyTimeSeries("IBM", "IPWCPZ1EZ7YVJRC5");
-			if (response == null) {
-				throw new RuntimeException("No Response from AlphaVantage");
+			ArrayList<MyTickerData> tickerData = new ArrayList<>();
+			for (String ticker : tickers) {
+				AlphaVantageTickerData response = service.getDailyTimeSeries(ticker, "IPWCPZ1EZ7YVJRC5");
+				if (response == null) {
+					throw new RuntimeException("No Response from AlphaVantage");
+				}
+				tickerData.add(response.convertToMyTickerData());
 			}
-			return response.convertToMyTickerData();
+			return tickerData;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
